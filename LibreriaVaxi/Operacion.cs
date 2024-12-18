aa61f568b477e936b52dde0831c2d7e67df91192
leogs-dev <leo.gs.dev@gmail.com>
2024-12-18 13:52:22 -0600

    Unit Test Course - MSTest

---
+++ LibreriaVaxi/Operacion.cs
@@ -0,0 +1,10 @@
+﻿namespace LibreriaVaxi
+{
+    public class Operacion
+    {
+        public int SumarNumeros(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+    }
+}
---
+++ LibreriaVaxiMSTest/OperacionMSTest.cs
@@ -0,0 +1,21 @@
+﻿using LibreriaVaxi;
+
+namespace LibreriaVaxiMSTest
+{
+    [TestClass]
+    public class OperacionMSTest
+    {
+        [TestMethod]
+        public void SumarNumeros_InputDosNumeros_GetValorCorrecto()
+        {
+            // 1. Arrange - Inicializacion de las variables o componentes que ejecutaran el test.
+            Operacion op = new Operacion();
+            int numero1Test = 50;
+            int numero2Test = 69;
+            // 2. Act - Representa la ejecucion de la operacion.
+            int resultado = op.SumarNumeros(numero1Test, numero2Test);
+            // 3. Assert - Propio del paquete Unit Test, contiene libreias y operadores para comparar tu resultado.
+            Assert.AreEqual(119, resultado);
+        }
+    }
+}
