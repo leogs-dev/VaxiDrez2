a35dd3c2f658a52a134e5ef2613197dbbddaf1f9
leogs-dev <leo.gs.dev@gmail.com>
2024-12-18 15:53:27 -0600

    Unit Test Course - NUnit

--- LibreriaVaxi/obj/Debug/net8.0/LibreriaVaxi.AssemblyInfo.cs
+++ LibreriaVaxi/obj/Debug/net8.0/LibreriaVaxi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibreriaVaxi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa61f568b477e936b52dde0831c2d7e67df91192")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibreriaVaxi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibreriaVaxi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ LibreriaVaxiNUnitTest/OperacionNUnitTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace LibreriaVaxi
+{
+    [TestFixture]
+    public class OperacionNUnitTest
+    {
+        [Test]
+        public void SumarNumeros_InputDosNumeros_GetValorCorrecto()
+        {
+            // 1. Arrange - Inicializacion de las variables o componentes que ejecutaran el test.
+            Operacion op = new Operacion();
+            int numero1Test = 50;
+            int numero2Test = 69;
+            // 2. Act - Representa la ejecucion de la operacion.
+            int resultado = op.SumarNumeros(numero1Test, numero2Test);
+            // 3. Assert - Propio del paquete Unit Test, contiene libreias y operadores para comparar tu resultado.
+            ClassicAssert.AreEqual(119, resultado);
+        }
+
+        [Test]
+        public void IsValorPar_InputNumeroImPar_ReturnFalse()
+        {
+            Operacion op = new Operacion();
+            int numeroImpar = 3;
+
+            bool isPar = op.IsValorPar(numeroImpar);
+            // Dos maneras de hacer la evaluacion.
+            // IsFalse() or That()
+            ClassicAssert.IsFalse(isPar);
+            Assert.That(isPar, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void IsValorPar_InputNumeroPar_ReturnTrue()
+        {
+            Operacion op = new Operacion();
+            int numeroPar = 8;
+
+            bool isPar = op.IsValorPar(numeroPar);
+
+            ClassicAssert.IsTrue(isPar);
+            Assert.That(isPar, Is.EqualTo(true));
+        }
+    }
+}
